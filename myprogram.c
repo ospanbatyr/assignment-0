af5ada7e13d1957a0f92635e9d0096c520b5b83e
Osman Batur İnce <47644681+ospanbatyr@users.noreply.github.com>
2024-02-18 13:19:41 +0300

    Add files via upload

---
+++ myprogram.c
@@ -0,0 +1,46 @@
+#include "mylibrary.h"
+
+int sum(int n)
+{
+	int result = 0;
+
+	//TODO: 1+2+...+n
+
+	return result;
+}
+
+int product_of_even_numbers(int *array, int count)
+{
+	int product = 1;
+	for (int i=0;i<count;++i)
+	{
+		//TODO: only multiply even numbers, e.g., 2. Skip odd numbers, e.g., 3
+	}
+	return product;
+}
+
+int max_of_numbers(int *array, int count)
+{
+	//TODO: return the maximum number from the array
+	int max = array[0];
+
+	return max;
+}
+
+int reversed_number(int number)
+{
+	//TODO: if input is 12345, return 54321
+	int reversed = 0;
+
+	return reversed;
+}
+
+
+char alphabet_index(int index)
+{
+	//TODO: for index 0, return A. index 1, B, etc. until 25 for Z.
+	//if index is out of range, return space ' '.
+	return ' ';
+}
+
+
